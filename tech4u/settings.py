5719919143827f8d98197b46fa62a3a5565df97e
rameez1822534 <ch_rameez89@hotmail.com>
2020-09-09 23:02:51 +0000

    static files  base header and nav created

--- tech4u/settings.py
+++ tech4u/settings.py
@@ -60,15 +60,15 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join (BASE_DIR, 'templates'),
-            os.path.join (BASE_DIR, 'templates', 'allauth'),
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth', # required by 'allauth'
+                'django.contrib.auth.context_processors.auth',  # required by 'allauth'
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -149,3 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
