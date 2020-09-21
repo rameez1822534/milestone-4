46a36221f190d8cb91ffd6fa9979f2d13a7eb9d0
rameez1822534 <ch_rameez89@hotmail.com>
2020-09-21 23:23:39 +0000

    json fixtures files loaded

--- products/admin.py
+++ products/admin.py
@@ -5,25 +5,5 @@ from .models import Product, Category
 # Register your models here.
 
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        'sku',
-        'name',
-        'category',
-        'price',
-        'rating',
-        'image',
-    )
-
-    ordering = ('sku',)
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'friendly_name',
-        'name',
-    )
-
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
+admin.site.register(Category)
