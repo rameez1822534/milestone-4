c2facc14a3c8b2861aa2aeccc9d578e9d5173d2b
rameez1822534 <ch_rameez89@hotmail.com>
2020-09-30 10:25:37 +0000

    models and webhook updats

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,11 +5,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
-<<<<<<< HEAD
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
-=======
-
-]
->>>>>>> d16bee38ca4036fddf23cd5521bd4dfd0ad610f9
